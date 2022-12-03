f39b38b17982fffea8cebe4215992c465c73619c
Micha Lehmann <michalehmann0112@gmail.com>
2022-12-04 00:25:38 +0100

    Measure execution time

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,27 @@
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+@OptIn(ExperimentalTime::class)
 fun main() {
     for (day in days) {
         println("======= ${day::class.simpleName} =======")
-        println("Puzzle1: ${day.solvePuzzle1()}")
-        println("Puzzle2: ${day.solvePuzzle2()}")
+
+        val time1 = measureTime {
+            print("Puzzle1: ${day.solvePuzzle1()} ")
+        }
+        println("(took $time1)")
+
+        val time2 = measureTime {
+            print("Puzzle1: ${day.solvePuzzle2()} ")
+        }
+        println("(took $time2)")
     }
 
-    println("Merry Christmas \uD83C\uDF84")
+    println("\uD83C\uDF84 Merry Christmas \uD83C\uDF84")
 }
 
 
-private val days = listOf<Day>(
+private val days = listOf(
     Day1(),
     Day2(),
     Day3()
