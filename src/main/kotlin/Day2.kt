702d982edcda37e90f7627f5bdf6af7acd1e086f
Micha Lehmann <michalehmann0112@gmail.com>
2022-12-03 23:09:34 +0100

    Refactor Day 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -13,20 +13,26 @@ import okio.Path.Companion.toPath
 
 class Day2 : Day {
     override fun solvePuzzle1() = runBlocking {
-        val instructions = loadInstructions("src/main/resources/day2.txt".toPath())
+        val instructions = loadInstructions("src/main/resources/day2.txt".toPath()).map {
+            (opponent, suggestion) -> opponent.toShape() to suggestion.toShape()
+        }
 
-        val scores = instructions.map { (opponent, player) ->
-            play(Shape.map[player]!!, Shape.map[opponent]!!)
+        val scores = instructions.map { (opponent, suggestion) ->
+            suggestion.value + suggestion.playAgainst(opponent).value
         }
 
         scores.reduce(Int::plus)
     }
 
     override fun solvePuzzle2() = runBlocking {
-        val instructions = loadInstructions("src/main/resources/day2.txt".toPath())
+        val instructions = loadInstructions("src/main/resources/day2.txt".toPath()).map { (opponent, suggestion) ->
+            opponent.toShape() to suggestion.toOutcome()
+        }
 
         val scores = instructions.map { (opponent, outcome) ->
-            play(selectShape(Shape.map[opponent]!!, Outcome.map[outcome]!!), Shape.map[opponent]!!)
+            val suggestion = outcome.achieveAgainst(opponent)
+
+            suggestion.value + suggestion.playAgainst(opponent).value
         }
 
         scores.reduce(Int::plus)
@@ -36,74 +42,64 @@ class Day2 : Day {
     private fun loadInstructions(path: Path)
         = readLines(FileSystem.SYSTEM.source(path)).map { line -> line.first() to line.last() }
 
-    private fun play(player: Shape, opponent: Shape): Int {
-        return when (player) {
-            Rock -> 1 + when (opponent) {
-                Rock -> 3
-                Paper -> 0
-                Scissors -> 6
-            }
+    private val shapeMap = mapOf(
+        'A' to Rock, 'B' to Paper, 'C' to Scissors,
+        'X' to Rock, 'Y' to Paper, 'Z' to Scissors
+    )
+    private fun Char.toShape() = shapeMap[this]!!
 
-            Paper -> 2 + when (opponent) {
-                Rock -> 6
-                Paper -> 3
-                Scissors -> 0
-            }
+    private val outcomeMap = mapOf('X' to Loss, 'Y' to Draw, 'Z' to Win)
+    private fun Char.toOutcome() = outcomeMap[this]!!
 
-            Scissors -> 3 + when (opponent) {
-                Rock -> 0
-                Paper -> 6
-                Scissors -> 3
+    private sealed class Shape(
+        val value: Int
+    ) {
+        object Rock : Shape(1) {
+            override fun playAgainst(other: Shape) = when (other) {
+                Scissors -> Win
+                Paper -> Loss
+                Rock -> Draw
             }
         }
+        object Paper : Shape(2) {
+            override fun playAgainst(other: Shape) = when (other) {
+                Rock -> Win
+                Scissors -> Loss
+                Paper -> Draw
+            }
+        }
+        object Scissors : Shape(3) {
+            override fun playAgainst(other: Shape) = when (other) {
+                Paper -> Win
+                Rock -> Loss
+                Scissors -> Draw
+            }
+        }
+
+        abstract fun playAgainst(other: Shape): Outcome
     }
 
-    private fun selectShape(opponent: Shape, outcome: Outcome): Shape {
-        return when (outcome) {
-            Win -> when (opponent) {
+    private sealed class Outcome(
+        val value: Int
+    ) {
+        object Win : Outcome(6) {
+            override fun achieveAgainst(other: Shape) = when (other) {
                 Rock -> Paper
                 Paper -> Scissors
                 Scissors -> Rock
             }
-
-            Loss -> when (opponent) {
+        }
+        object Loss : Outcome(0) {
+            override fun achieveAgainst(other: Shape) = when (other) {
                 Rock -> Scissors
                 Paper -> Rock
                 Scissors -> Paper
             }
-
-            Draw -> opponent
         }
-    }
-
-    private enum class Shape {
-        Rock,
-        Paper,
-        Scissors;
-
-        companion object {
-            val map = mapOf(
-                'A' to Rock,
-                'B' to Paper,
-                'C' to Scissors,
-                'X' to Rock,
-                'Y' to Paper,
-                'Z' to Scissors
-            )
+        object Draw : Outcome(3) {
+            override fun achieveAgainst(other: Shape) = other
         }
-    }
 
-    private enum class Outcome {
-        Win,
-        Loss,
-        Draw;
-
-        companion object {
-            val map = mapOf(
-                'X' to Loss,
-                'Y' to Draw,
-                'Z' to Win
-            )
-        }
+        abstract fun achieveAgainst(other: Shape): Shape
     }
 }
